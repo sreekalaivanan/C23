3d3a9017c148683bb8cbaa26d51b322722779405
sreekalaivanan <73590949+sreekalaivanan@users.noreply.github.com>
2020-11-28 10:25:43 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,34 @@
+//Namespacing
+
+const Engine=Matter.Engine;  //universe
+const World=Matter.World; //globe
+const Bodies= Matter.Bodies; //objects
+
+var engine,world,box1,box2,ground;
+
+
+function setup() {
+  createCanvas(400,400);
+
+engine=Engine.create();   // create the Universe
+world = engine.world;
+
+box1= new Box(200,300,50,50);
+box2=new Box(240,100,50,100);
+
+ground = new Ground(200,390,600,30);
+
+
+}
+
+function draw() {
+  background(0,0,0);  
+
+  Engine.update(engine);
+
+  box1.display();
+  box2.display();
+  ground.display();
+  
+
+}
